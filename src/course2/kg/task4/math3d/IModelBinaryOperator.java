daf88a0d13c3075e4f06edec9503cd815d85c7fa
Victor Valeev <viktorval2001@gmail.com>
2020-11-23 00:41:18 +0300

    Initial commit

---
+++ src/course2/kg/task4/math3d/IModelBinaryOperator.java
@@ -0,0 +1,18 @@
+package course2.kg.task4.math3d;
+
+import course2.kg.task4.third.IModel;
+
+public class IModelBinaryOperator {
+
+    public static IModel merge(IModel m1, IModel m2) {
+        return null;
+    }
+
+    public static IModel exclude(IModel from, IModel that) {
+        return null;
+    }
+
+    public static IModel getCommon(IModel m1, IModel m2) {
+        return null;
+    }
+}
